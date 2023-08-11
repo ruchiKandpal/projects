b4c41c0bc559bc36c5692cb7c6a465db2e2713f9
ruchiKandpal <kandpalruchi0614@gmail.com>
2023-08-11 13:07:56 +0530

    11th August work

---
+++ quiz.py
@@ -0,0 +1,31 @@
+print("Its a quiz")
+
+marks = 0
+
+print("QUestion1")
+q1 = input("What is the name of our country")
+if(q1=="india"):
+    marks = marks+1
+    
+
+print("Question2")
+q2 = input("What is the name of our National Animal")
+if(q2=="tiger"):
+    marks = marks+1
+
+print("Question3")
+q3 = input("What is our National Bird")
+if(q3=="pecock"):
+    marks = marks+1
+
+print("Question4")
+q4 = input("What is our National Flower")
+if(q4=="lotus"):
+    marks = marks+1
+
+print("Question5")
+q5 = input("What is our National Game")
+if(q5=="hockey"):
+    marks = marks+1
+
+print(marks)
